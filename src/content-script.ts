ae9ea73973c549df09bc8a61fda1d2f908c22514
Guilherme Félix <guilherme.f.santos@outlook.com.br>
2023-10-24 17:19:19 -0300

    Update content-script.ts to detect classes containing the word 'search'

--- src/content-script.ts
+++ src/content-script.ts
@@ -1,5 +1,5 @@
 let possibleSearchInputs = document.querySelectorAll(
-	`input[type="search"], textarea[type="search"], input[name*="search"], textarea[name*="search"], input[aria-label*="Search"], textarea[aria-label*="Search"], input[id*="search"], textarea[id*="search"], input[placeholder*="Search"], textarea[placeholder*="Search"], input[data-search="true"], textarea[data-search="true"]`
+	`input[type="search"], textarea[type="search"], input[name*="search"], textarea[name*="search"], input[aria-label*="Search"], textarea[aria-label*="Search"], input[id*="search"], textarea[id*="search"], input[class*="search"], textarea[class*="search"], input[placeholder*="Search"], textarea[placeholder*="Search"], input[data-search="true"], textarea[data-search="true"]`
 ) as NodeListOf<HTMLInputElement>;
 
 document.addEventListener('keypress', keyEvent => {

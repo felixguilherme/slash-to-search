2888a2896eb82cd9345a6153f632763d85a52fac
Guilherme Félix <guilherme.f.santos@outlook.com.br>
2023-10-25 22:00:48 -0300

    Update content-script.ts logic

--- src/content-script.ts
+++ src/content-script.ts
@@ -1,10 +1,18 @@
-let possibleSearchInputs = document.querySelectorAll(
-	`input[type="search"], textarea[type="search"], input[name*="search"], textarea[name*="search"], input[aria-label*="Search"], textarea[aria-label*="Search"], input[id*="search"], textarea[id*="search"], input[class*="search"], textarea[class*="search"], input[placeholder*="Search"], textarea[placeholder*="Search"], input[data-search="true"], textarea[data-search="true"]`
-) as NodeListOf<HTMLInputElement>;
+function getPossibleSearchInputs() {
+	const possibleSearchInputs = document.querySelectorAll(
+		`input[type="search"], textarea[type="search"], input[name*="search"], textarea[name*="search"], input[aria-label*="Search"], textarea[aria-label*="Search"], input[id*="search"], textarea[id*="search"], input[class*="search"], textarea[class*="search"], input[placeholder*="Search"], textarea[placeholder*="Search"], input[data-search="true"], textarea[data-search="true"]`
+	) as NodeListOf<HTMLInputElement>;
+
+	return possibleSearchInputs;
+}
 
 document.addEventListener('keypress', keyEvent => {
 	if (keyEvent.key === '/') {
-		keyEvent.preventDefault();
-		possibleSearchInputs[0].focus();
+		const inputsFound = getPossibleSearchInputs();
+		console.log(inputsFound);
+		if (inputsFound.length > 0) {
+			keyEvent.preventDefault();
+			inputsFound[0].focus();
+		}
 	}
 });
